f6cf11b49243702e119135354876a2b4bf68ad9e
Vitoria Regina <vitoriasantos78690@gmail.com>
2023-08-23 16:18:32 -0300

    tela de aviso

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import {} from "react";
+import React, { useState } from "react";
 import Logo from "./img/logoSisyphus.png";
 import "./index.scss";
 import Estudante from "./img/estudante.png";
 
 function App() {
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  if (modal) {
+    document.body.classList.add("active-modal");
+  } else {
+    document.body.classList.remove("active-modal");
+  }
+
   return (
     <>
       <div className="menu">
@@ -21,13 +33,28 @@ function App() {
           Monitore suas tarefas pessoais e de toda a equipe com mais facilidade
           conosco.
         </p>
-
         <input className="input-email" type="text" placeholder="Email" />
-
-        <button className="button-email">Enviar</button>
+        <button onClick={toggleModal} className="button-email">
+          Enviar
+        </button>
       </div>
 
       <img className="img-estudante" src={Estudante} alt="Estudante" />
+
+      {modal && (
+        <div className="modal">
+          <div onClick={toggleModal} className="overlay"></div>
+          <div className="modal-content">
+            <h2>Sucesso!</h2>
+            <p className="p-modal">
+              Email enviado. Aguarde, entraremos em contato em breve.
+            </p>
+            <button className="close-modal" onClick={toggleModal}>
+              Fechar
+            </button>
+          </div>
+        </div>
+      )}
     </>
   );
 }
